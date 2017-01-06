6e55a0bce2a9614938167f1592d177604a7f9e04
denglevi <denglevi@hotmail.com>
2017-01-06 17:23:27 +0800

    ----------------

--- src/mainLayer.js
+++ src/mainLayer.js
@@ -3,15 +3,15 @@
  */
 var MainLayer = cc.Layer.extend({
     sprite: null,
-    wordInfo:null,
-    keyword:'',
+    wordInfo: null,
+    keyword: '',
     ctor: function () {
         this._super();
 
         this.getWordInfo();
         var size = cc.winSize;
 
-        this.clearLabel = new cc.LabelTTF("clear", "Arial", 38);
+        this.clearLabel = new cc.LabelTTF("reset", "Arial", 38);
         this.clearLabel.x = size.width - 55;
         this.clearLabel.y = size.height / 10;
 
@@ -31,27 +31,27 @@ var MainLayer = cc.Layer.extend({
 
         return true;
     },
-    getWordInfo:function(){
+    getWordInfo: function () {
         cc.log(1253);
         var xhr = cc.loader.getXMLHttpRequest();
-        xhr.open("GET", "http://wordgame.denglevi.com/getWordList");
-        xhr.setRequestHeader("Content-Type","text/plain;charset=UTF-8");
+        xhr.open("GET", res.Host + "/getWordList");
+        xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
         var mainLayer = this;
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && (xhr.status >= 200 && xhr.status <= 207)) {
                 var httpStatus = xhr.statusText;
                 var response = xhr.responseText;
                 // try{
-                    mainLayer.wordInfo = JSON.parse(response);
-                    if(mainLayer.wordInfo.length <= 0){
-                        var size = cc.winSize;
-                        var noticeLabel = new cc.LabelTTF('获取数据错误', "Arial", 38);
-                        noticeLabel.x = size.width / 2;
-                        noticeLabel.y = size.height / 2;
-                        mainLayer.addChild(noticeLabel,1000);
-                        return false;
-                    }
-                    mainLayer.showWordInfo(mainLayer.wordInfo[0]);
+                mainLayer.wordInfo = JSON.parse(response);
+                if (mainLayer.wordInfo.length <= 0) {
+                    var size = cc.winSize;
+                    var noticeLabel = new cc.LabelTTF('获取数据错误', "Arial", 38);
+                    noticeLabel.x = size.width / 2;
+                    noticeLabel.y = size.height / 2;
+                    mainLayer.addChild(noticeLabel, 1000);
+                    return false;
+                }
+                mainLayer.showWordInfo(mainLayer.wordInfo[0]);
                 // }catch (e){
                 //     console.error("无法解析服务器的响应结果: \n" + response);
                 // }
@@ -60,8 +60,8 @@ var MainLayer = cc.Layer.extend({
         };
         xhr.send();
     },
-    showWordInfo:function(obj){
-        cc.audioEngine.playEffect('http://wordgame.denglevi.com/assets/actor.wav');
+    showWordInfo: function (obj) {
+        cc.audioEngine.playEffect(res.Host + '/assets/actor.wav');
         var size = cc.winSize;
         var mainLayer = this;
         var wordLabel = new cc.LabelTTF(obj.word, "Arial", 38);
@@ -69,16 +69,35 @@ var MainLayer = cc.Layer.extend({
         wordLabel.y = size.height / 2 + 200;
         this.addChild(wordLabel, 5);
 
+        var len = obj.wordZH.length;
+        var wordNum = parseInt(size.width/20);
+        cc.log(wordNum);
+        var newWordZH = '';
+        if (len > wordNum) {
+            for (var i = 0; i < len; i++) {
+                if (i % wordNum == 0 && i != 0) newWordZH += obj.wordZH[i] + "\n";
+                else newWordZH += obj.wordZH[i];
+                console.log(newWordZH);
+            }
+        } else {
+            newWordZH = obj.wordZH;
+        }
+        var wordZHLabel = new cc.LabelTTF(newWordZH, "Arial", 20);
+        wordZHLabel.x = size.width / 2;
+        wordZHLabel.y = size.height / 2 + 160;
+        this.addChild(wordZHLabel, 5);
+
         // var wordZH = obj.wordZH.split(',');
+        // var testWord = obj.word;
         // obj.word = obj.word+'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
         var keyword = '';
-        var valNum = setInterval(function(){
-            var i = parseInt(Math.random()*obj.word.length);
+        var valNum = setInterval(function () {
+            var i = parseInt(Math.random() * obj.word.length);
             var r = parseInt(Math.random() * 255);
             var g = parseInt(Math.random() * 255);
             var b = parseInt(Math.random() * 255);
             var random_num = parseInt(50 + Math.random() * 350);
-            var num = new cc.LabelTTF(obj.word[i], "Helvetica-Bold", 60);
+            var num = new cc.LabelTTF(obj.word[i].toUpperCase(), "Helvetica-Bold", 60);
             num.setFontFillColor(cc.color(r, g, b, 0));
             num.x = random_num;
             num.y = 100;
@@ -87,7 +106,7 @@ var MainLayer = cc.Layer.extend({
 
             var scale = cc.scaleTo(1, 1);
             var fadeOut = cc.fadeOut(3);
-            var rotate = cc.rotateBy(parseInt(Math.random()*5),360+parseInt(Math.random()*360));
+            var rotate = cc.rotateBy(parseInt(Math.random() * 5), 360 + parseInt(Math.random() * 360));
             var func = cc.callFunc(function (data) {
                 data.removeFromParent(true);
             }, mainLayer, num);
@@ -100,32 +119,70 @@ var MainLayer = cc.Layer.extend({
             num.runAction(seq);
 
             cc.eventManager.addListener(cc.EventListener.create({
-                event:cc.EventListener.TOUCH_ONE_BY_ONE,
-                onTouchBegan:function(touch,event){
+                event: cc.EventListener.TOUCH_ONE_BY_ONE,
+                onTouchBegan: function (touch, event) {
                     var target = event.getCurrentTarget();
                     var pos = touch.getLocation();
 
                     if (cc.rectContainsPoint(target.getBoundingBox(), pos)) {
+                        target.removeFromParent(true);
+                        var moveOutSprite = new cc.Sprite(res.Beams_png, cc.rect(160, 209, 40, 90));
+                        moveOutSprite.attr({
+                            x: pos.x,
+                            y: pos.y
+                        });
+                        var scale = cc.scaleTo(0.5, 0.1, 0.1);
+                        var move = cc.moveTo(0.5, moveOutSprite.x, moveOutSprite.y + 400);
+                        var func = cc.callFunc(function (data) {
+                            data.removeFromParent(true);
+                        }, moveOutSprite, null);
+                        var seq = cc.sequence(move, func);
+                        moveOutSprite.runAction(seq);
+                        moveOutSprite.runAction(scale);
+
+                        mainLayer.addChild(moveOutSprite, 1000);
+
                         keyword += num.getString();
                         cc.log(keyword);
                         if (keyword.toLowerCase() == obj.word.toLowerCase()) {
                             wordLabel.removeFromParent(true);
+                            var flareSprite = new cc.Sprite(res.Flare_png);
+                            flareSprite.attr({
+                                x: wordLabel.x,
+                                y: wordLabel.y,
+                                scale: (0.6, 0.6)
+                            });
+
+                            var controlPoints = [cc.p(wordLabel.x - 200, wordLabel.y), cc.p(wordLabel.x - 100, wordLabel.y + 50), cc.p(wordLabel.x + 150, wordLabel.y + 150)];
+                            var func = cc.callFunc(function (obj) {
+                                obj.removeFromParent(true);
+                            }, flareSprite, null);
+                            var scale = cc.scaleTo(2, 0.1, 0.1);
+                            var bezierTo = cc.bezierTo(2, controlPoints);
+                            var rep = cc.sequence(bezierTo, func);
+                            var rotate = cc.rotateBy(2, 720);
+
+                            flareSprite.runAction(rep);
+                            flareSprite.runAction(scale);
+                            flareSprite.runAction(rotate);
+                            mainLayer.addChild(flareSprite, 15000);
+
                             keyword = '';
                             clearInterval(valNum);
                             var indexNum = mainLayer.wordInfo.indexOf(obj);
-                            if(indexNum == mainLayer.wordInfo.length-1){
+                            if (indexNum == mainLayer.wordInfo.length - 1) {
                                 cc.log("完成任务");
                                 return false;
-                            }else{
-                                var wordObj = mainLayer.wordInfo[indexNum+1]
+                            } else {
+                                var wordObj = mainLayer.wordInfo[indexNum + 1];
                                 mainLayer.showWordInfo(wordObj);
                             }
                         }
                         return true;
                     }
                 }
-            }),num);
-        },500);
+            }), num);
+        }, 300);
 
         cc.eventManager.addListener(cc.EventListener.create({
             event: cc.EventListener.TOUCH_ONE_BY_ONE,
@@ -138,6 +195,6 @@ var MainLayer = cc.Layer.extend({
                     keyword = "";
                 }
             }
-        }),mainLayer.clearLabel);
+        }), mainLayer.clearLabel);
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -2,6 +2,9 @@ var res = {
     Logo_png : "res/logo.png",
     Bg_png : "res/bg.png",
     Actor_wav : "res/actor.wav",
+    Beams_png : "res/beams.png",
+    Flare_png:'res/flare.png',
+    Host:'http://10.33.52.41'
 };
 
 var g_resources = [];
